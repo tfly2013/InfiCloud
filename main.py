221bd2e04de9dfa67b38933bc65a7d5ad09c4272
Fei Tang <tfly2012@hotmail.com>
2016-04-26 23:41:56 +1000

    Save tweets into couchDB, and the couchDB library

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
+import couchdb
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+DB_NAME = 'demo'
+KEYWORD = 'Melbourne'
 
 # Variables that contains the user credentials to access Twitter API 
 ACCESS_TOKEN = '724923138233012224-CtQQ4qB08Cx0ubb8wTi3Hlu5M9uoZMP'
@@ -7,28 +10,28 @@ ACCESS_SECRET = '7nyzJpJNi3ojCW63tPM7h7n7qXwExeZqcar4ZO7YpID6P'
 CONSUMER_KEY = 'KYCiQNaYLBOlPRm0YIrALqgKG'
 CONSUMER_SECRET = 'FXJKLs7Ft7DvcF0OAIbtScy5n5bn19tnyQpYswDkvvZkt1SUSm'
 
+# Set up couch db
+couch = couchdb.Server("http://115.146.94.116:5984/")
+db = couch[DB_NAME]
+
+# Twitter API authentication
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
 # Initiate the connection to Twitter Streaming API
 twitter_stream = TwitterStream(auth=oauth)
 
 # Get a sample of the public data following through Twitter
-iterator = twitter_stream.statuses.filter(track="Melbourne", language="en")
+iterator = twitter_stream.statuses.filter(track= KEYWORD, language="en")
 
-# Print each tweet in the stream to the screen 
-# Here we set it to stop after getting 1000 tweets. 
-# You don't have to set it to stop, but can continue running 
-# the Twitter API to collect data for days or even longer. 
-tweet_count = 1000
+tweet_count = 10
 for tweet in iterator:
     tweet_count -= 1
-    # Twitter Python Tool wraps the data returned by Twitter 
-    # as a TwitterDictResponse object.
-    # We convert it back to the JSON format to print/score
-    print(json.dumps(tweet))
+
+    # INSERT PROCESSING HERE
+
+    # Save tweet into database
+    db.save(tweet)
     
-    # The command below will do pretty printing for JSON data, try it out
-    # print json.dumps(tweet, indent=4)
-       
     if tweet_count <= 0:
-        break 
+        break
+    
