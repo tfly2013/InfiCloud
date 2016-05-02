a2afe53626d46bdd35cdfcc29b6a540438c60826
Kimple Ke <kimpleke@WSPs-MacBook-Pro.local>
2016-05-02 14:28:53 +1000

    format correction

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -5,10 +5,10 @@ couch = couchdb.Server("http://115.146.94.116:5984/")
 db = couch["melbourne"]
 count = 0
 for doc_id in db:
-        doc = db[doc_id]
-        if doc["id"] in ids:
-                db.delete(doc)
-                count += 1
-        else:
-                ids.append(doc["id"])
+    doc = db[doc_id]
+    if doc["id"] in ids:
+        db.delete(doc)
+        count += 1
+    else:
+        ids.append(doc["id"])
 print(count)
