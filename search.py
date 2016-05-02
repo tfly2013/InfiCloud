233fb289c702c53d0eebf1773d1b138a1e826ec3
Kimple Ke <kimpleke@WSPs-MacBook-Pro.local>
2016-05-02 15:02:33 +1000

    quick and easy format fix

--- search.py
+++ search.py
@@ -7,24 +7,24 @@ from twitter import Twitter, OAuth, TwitterHTTPError
 token = [
     # Fei's token
     ['724923138233012224-CtQQ4qB08Cx0ubb8wTi3Hlu5M9uoZMP',
-    '7nyzJpJNi3ojCW63tPM7h7n7qXwExeZqcar4ZO7YpID6P',
-    'KYCiQNaYLBOlPRm0YIrALqgKG',
-    'FXJKLs7Ft7DvcF0OAIbtScy5n5bn19tnyQpYswDkvvZkt1SUSm'],
+     '7nyzJpJNi3ojCW63tPM7h7n7qXwExeZqcar4ZO7YpID6P',
+     'KYCiQNaYLBOlPRm0YIrALqgKG',
+     'FXJKLs7Ft7DvcF0OAIbtScy5n5bn19tnyQpYswDkvvZkt1SUSm'],
     # Roger's token
     ['2172130051-DFJB7TToHvJSMU6iYRs62zTxXHbZTNX9Y4Y5wx3',
-    '1KEsYxYYhd79hVWf8GQslU4GMkpmZLsj03M8vVIpcG4fb',
-    'D8JQLo7N5v9jTr8C6tqgvRl7t',
-    'UvvzGLHrGYeFGsL7AJ0QJl4p8WOkYeJE36aPwOTOZJwHvT54g2'],
+     '1KEsYxYYhd79hVWf8GQslU4GMkpmZLsj03M8vVIpcG4fb',
+     'D8JQLo7N5v9jTr8C6tqgvRl7t',
+     'UvvzGLHrGYeFGsL7AJ0QJl4p8WOkYeJE36aPwOTOZJwHvT54g2'],
     # Kimple's token
     ['1879356786-obaH7zmTg2ws0Pi6mY6JbFMErMjqKxzbBGuH9ZY',
-    'pI6Wnj9CJDIscF5cVVoVe8tWJ396jITVHNOai4deWTW5l',
-    'ZsIYOPOghaoCIfj4BG76S5fGw',
-    'lcffivvg1gSiMkvN0dmHXuce3fcBoWeyr8qmSmokxm3aSIF3dq'],
+     'pI6Wnj9CJDIscF5cVVoVe8tWJ396jITVHNOai4deWTW5l',
+     'ZsIYOPOghaoCIfj4BG76S5fGw',
+     'lcffivvg1gSiMkvN0dmHXuce3fcBoWeyr8qmSmokxm3aSIF3dq'],
     # Bofan's Token
     ['724926978873298946-GzUWebFMysrG5c9B0QLCKwUIS334Ff2',
-    'TkQgu1VyUb9OsfYn8XJIEW3DSn9ROEqdjRdn39gCrUF39',
-    'FiwgzSsEsYA9p5KjCnzr82Lhw',
-    'AQWlN7Zwpm4rYWIV2krL81jYBKH1Nj0LFc6hfkYAALfC691hCR']
+     'TkQgu1VyUb9OsfYn8XJIEW3DSn9ROEqdjRdn39gCrUF39',
+     'FiwgzSsEsYA9p5KjCnzr82Lhw',
+     'AQWlN7Zwpm4rYWIV2krL81jYBKH1Nj0LFc6hfkYAALfC691hCR']
 ]
 
 dbname = ["melbourne", "sydney", "brisbane", "perth"]
@@ -77,7 +77,7 @@ def harvest(args, lexicon):
         userID = doc["user"]["id"]
         if "searched" not in doc:
             doc["searched"] = True
-            inputDB.save(doc)            
+            inputDB.save(doc)
             if userID not in ids:
                 ids.add(userID)
                 result = t.statuses.user_timeline(user_id=userID, count=200)
