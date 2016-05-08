a57ef9a04a8a9b494edb91f0f21611786056fde2
Fei Tang <tfly2012@hotmail.com>
2016-05-08 10:55:08 +1000

    update remove duplicate using set

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -1,14 +1,12 @@
 import couchdb
 
-ids = []
+ids = set()
 couch = couchdb.Server("http://115.146.94.116:5984/")
 db = couch["melbourne"]
-count = 0
 for doc_id in db:
     doc = db[doc_id]
-    if doc["id"] in ids:
+	id = doc["id"]
+    if id in ids:
         db.delete(doc)
-        count += 1
     else:
-        ids.append(doc["id"])
-print(count)
+        ids.add(id)
