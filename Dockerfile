682c0749bf02ae997c4c358e156d695938daf057
Shuliyey <Shuliyey@gmail.com>
2016-05-11 01:40:30 +1000

    add gulp task for docker package

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM ubuntu:xenial
 
-RUN apt-get install -y python-software-properties python-pip python-dev build-essential
-RUN pip install ntlk twitter couchdb
+RUN apt-get update
+RUN apt-get install -y software-properties-common python-pip python-dev build-essential
+RUN pip install nltk twitter couchdb
 RUN mkdir -p /root/comp90024_twitter_miner
 
-ADD dist /root/comp90024_twitter_miner
+ADD dist/* /root/comp90024_twitter_miner/
+ADD Dockerfile /root/comp90024_twitter_miner/
 
 WORKDIR /root/comp90024_twitter_miner
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
+var argv = require('yargs')
+  .default('tag', "v1.0")
+  .alias('t', 'tag')
+  .argv;
+
+console.log(argv.tag);
 
 gulp.task('build', shell.task([
   'python -m compileall *.py'
 ]));
 
 gulp.task('dist', ['build'], function () {
-  gulp.src("*.pyc")
+  gulp.src(["*.pyc", "*.csv"])
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['build', 'dist']);
+
+gulp.task('package', ['default'], shell.task([
+  'docker build -t shuliyey/twitter_miner:' + argv.tag + ' .',
+  'docker push shuliyey/twitter_miner:' + argv.tag
+]));
